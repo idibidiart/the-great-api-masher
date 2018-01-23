768a633890aa383f3fbb0a7ef8c7dc9b7fb3c82c
idibidiart <marc.fawzi@gmail.com>
2018-01-23 07:24:43 -0800

    updated Readme

---
+++ src/data-source-mock/resolvers.1.ts
@@ -0,0 +1,25 @@
+import Model from './model'
+import Connector from './connector';
+
+const model = new Model({ connector: new Connector() })
+
+export default {
+  Query: {
+    greenApple: (parent, args, context) => model.getData({type: "GreenApple"}),
+    cherry: (parent, args, context) => model.getData({type: "Cherry"}),
+    fruit: (parent, args, context) => model.getData({}), // returns Union of both types
+  }, 
+  // GraphQL must be able to distinguish GreenApple from Cherry in MixedFruit
+  // which is a Union of different types (i.e. the actual type is **not** fixed 
+  // at design time) 
+  // We do this with __resolveType
+  MixedFruit: {
+    __resolveType(obj) {
+        if (obj.cherry)  {
+            return "Cherry"
+        } else {
+            return "GreenApple"
+        }
+    }
+  }
+};
